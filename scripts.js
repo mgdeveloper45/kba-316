8bb3908cd6dd9dceebcf61c2dcb378675b15466a
MarkGee45 <moel55.mg@gmail.com>
2024-11-14 12:23:39 -0800

    transferred code from 307 and cache elements

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const countrySelect = document.getElementById("countrySelect");
+const newsletterForm = document.querySelector("#newsletterForm");
+const nameInput = document.querySelector("#nameInput");
+const emailInput = document.querySelector("#emailInput");
