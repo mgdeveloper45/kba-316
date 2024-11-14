5b2f6bba7a90d4d7f781e8470c763f758adfbcf9
MarkGee45 <moel55.mg@gmail.com>
2024-11-14 12:46:31 -0800

    created event listener for DOM interactions

--- scripts.js
+++ scripts.js
@@ -2,3 +2,10 @@ const countrySelect = document.getElementById("countrySelect");
 const newsletterForm = document.querySelector("#newsletterForm");
 const nameInput = document.querySelector("#nameInput");
 const emailInput = document.querySelector("#emailInput");
+
+countrySelect.addEventListener("change", (evt) => {
+    evt.preventDefault();
+    if(countrySelect.value !== "default") {
+        alert(`You selected ${countrySelect.options[countrySelect.selectedIndex].text}!`);
+    }
+})
