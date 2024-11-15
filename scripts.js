a2928f0cc8f8b68bd0ba1063132f3b19e491939d
MarkGee45 <moel55.mg@gmail.com>
2024-11-15 00:19:30 -0800

    created event listener to toggle destinations

--- scripts.js
+++ scripts.js
@@ -3,17 +3,22 @@ const newsletterForm = document.querySelector("#newsletterForm");
 const nameInput = document.querySelector("#nameInput");
 const emailInput = document.querySelector("#emailInput");
 
-countrySelect.addEventListener("change", (evt) => {
-    evt.preventDefault();
+countrySelect.addEventListener("change", () => {
     if(countrySelect.value !== "default") {
         alert(`You selected ${countrySelect.options[countrySelect.selectedIndex].text}!`);
     }
 
-    newsletterForm.addEventListener("submit", evt => {
-        evt.preventDefault();
-        if(nameInput.value && emailInput.value) {
-            alert("Thank you for subscribing!");
-            newsletterForm.reset();
-        }
-    })
+})
+newsletterForm.addEventListener("submit", evt => {
+    evt.preventDefault();
+    if(nameInput.value && emailInput.value) {
+        alert("Thank you for subscribing!");
+        newsletterForm.reset();
+    }
+})
+
+countrySelect.addEventListener("change", () => {
+    if(countrySelect.value !== "default") {
+        window.location.href = `destinations.html#${countrySelect.value}`
+    }
 })
